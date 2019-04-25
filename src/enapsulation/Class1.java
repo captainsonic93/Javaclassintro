d44a1ce452806af5714118b7abb9750b9dc342ca
sloanthomas <sloanthomassr@gmail.com>
2019-04-25 11:22:12 -0500

    Encapsulation

---
+++ src/enapsulation/Class1.java
@@ -0,0 +1,32 @@
+package enapsulation;
+
+public class Class1 {
+
+    private String face;
+    private int time;
+    private int age;
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/enapsulation/Class2.java
@@ -0,0 +1,8 @@
+package enapsulation;
+
+public class Class2 {
+
+    private String myName = "sloan";
+    private int rolled = 34;
+    private int stand = 27;
+}
